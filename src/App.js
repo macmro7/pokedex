c22e3c2ca9d4fe7c0e81bb8f1169c76d5d3b612a
Maciej <macmro3@st.amu.edu.pl>
2022-04-13 12:02:48 +0200

    fetch data from API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import { useState, useEffect } from 'react'
 import './App.css';
 import Navbar from './Components/Navbar.jsx'
 
 function App() {
+  const [ pokemonData, setPokemonData ] = useState()
+
+  async function getData() {
+    const data = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/`
+    ).then(response => response.json());
+    setPokemonData(data.results)
+    console.log(pokemonData)
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+
   return (
     <div className="App">
       <Navbar />
+      { pokemonData && pokemonData.map(pokemon => <li>{pokemon.name}</li>) }
     </div>
   );
 }
