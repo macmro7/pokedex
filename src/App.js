d95eb84f457053e5292c071a33de1b1b7019798b
Maciej <macmro3@st.amu.edu.pl>
2022-04-13 10:47:49 +0200

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Navbar from './Components/Navbar.jsx'
 
 function App() {
   return (
     <div className="App">
-      <h1></h1>
+      <Navbar />
     </div>
   );
 }
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar() {
+    return (
+        <header>
+            <h1>Pokedex</h1>
+        </header>
+    )
+}
+
+export default Navbar
