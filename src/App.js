b2f4c6b631a3ce910a4539b70fc4ab6ffe32ba6d
Maciej <macmro3@st.amu.edu.pl>
2022-04-14 12:30:06 +0200

    add handling fetch errors, fix loading message

--- src/App.js
+++ src/App.js
@@ -5,78 +5,120 @@ import Pokemon from './Components/Pokemon.jsx'
 
 function App() {
   const [ pokemonData, setPokemonData ] = useState()
-  const [ isLoaded, setIsLoaded ] = useState(false)
+  const [ isLoading, setIsLoading ] = useState(true)
   const [ url, setUrl] = useState(`https://pokeapi.co/api/v2/pokemon/`)
   const [ nextUrl, setNextUrl ] = useState()
   const [ prevUrl, setPrevUrl ] = useState()
-  const [pokemon, setPokemon ] = useState()
-  const [error, setError] = useState(null);
-  const [ list, setList ] = useState([])
+  const [ pokemonName, setPokemonName ] = useState()
+  const [ error, setError ] = useState()
   //const [ pokemonList, setPokemonList ] = useState()
   const pokemonList = []
 
-  async function getData() {
-    const data = await fetch(url)
+  /*async function getData() {
+    const data = await fetch(`${url + pokemonName}`)
     .then(response => response.json());
-    setPokemonData(data.results) 
+
+    if(data.results) {
+      setPokemonData(data.results) 
+      setNextUrl(data.next)
+      setPrevUrl(data.previous)
+    } 
+    else {  // allows filtering by name
+      setPokemonData([data]) 
+    }
+
     //getPokemonData(data.results)
-    setNextUrl(data.next)
-    setPrevUrl(data.previous)
-  }
-
-  /*function getPokemonData(results) {
-    //setPokemonData([]) // delete this
-    const pokemons = []
-    results.map(async pokemon => {
-      const data = await fetch(
-        `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
-        .then(res => res.json())
-
-      }, setIsLoaded(true))
-    setList(pokemons)
   }*/
 
+  async function getData() {
+    if(pokemonName) {
+      await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+      .then(res => {
+        if(!res.ok) {
+          setPokemonData(null)
+          setIsLoading(null)
+          throw Error(`Pokemon ${pokemonName} could not be found`)
+        }
+        return res.json()
+      })
+      .then(data => {
+        setPokemonData([data])
+        setIsLoading(false)
+        setError('')
+      })
+      .catch(err => {
+        setError(err.message)
+      })
+    }
+    else {
+      await fetch(url)
+      .then(res => {
+        if(!res.ok) {
+          throw Error(`Error`)
+        }
+        return res.json()
+      })
+      .then(data => {
+        setPokemonData(data.results)
+        setNextUrl(data.next)
+        setPrevUrl(data.previous)
+
+        setIsLoading(false)
+        setError('')
+      })
+      .catch(err => {
+        setError(err.message)
+      })
+    }
+  }
+
+
   function loadNext() {
+    setIsLoading(true)
     setUrl( nextUrl )
+    console.log(nextUrl)
   }
 
   function loadPrevious() {
+    setIsLoading(true)
     prevUrl && setUrl( prevUrl )
+  }
+
+  function handleSubmit(name) {
+    setIsLoading(true)
+    setError(null)
+    name && setPokemonName(name)
+  }
+
+  function refresh(){
+    setUrl(`https://pokeapi.co/api/v2/pokemon/`)
+    setPokemonName('')
+    setError('')
   }
 
   useEffect(() => {
     getData()
-  }, [url])
+  }, [url, pokemonName])
 
-  /*useEffect(() => {
-    setPokemonData(list)
-    console.log('tutaj')
-  }, [list])*/
-
-  //console.log(pokemonData)
-
-  //<img src={item.sprites.other["official-artwork"].front_default} alt="pokemon"/>
-  // <button onClick={loadMore}>Load more pokemons</button>
-  // { isLoaded && pokemonData ? pokemonData.map(item => (
-  //   <li key={item.id}>
-  //   <img src={item.sprites.front_default} alt="pokemon"/>
-  //   {item.name} 
-  //   {item.types[0].type.name}
-  // </li>)) 
-  // : 'Loading...' }
-
-  //console.log(pokemonData)
+  // {error ? error :
+  //   (<div className="pokemon--list">
+  //     {pokemonData ? pokemonData.map((pokemon) => (
+  //       <Pokemon key={pokemon.name} name={pokemon.name} />)) 
+  //       : 'Loading...' }
+  //   </div>)
+  // } 
   return (
     <div className="App">
-      <Navbar />
+      <Navbar handleSubmit={handleSubmit} refresh={refresh} />
       <div className="pagination">
         <button onClick={loadPrevious}>Previous page</button>
         <button onClick={loadNext}>Next page</button>
       </div>
       <div className="pokemon--list">
-        {pokemonData ? pokemonData.map((pokemon) => (
-          <Pokemon key={pokemon.name} name={pokemon.name} />)) 
-          : 'Loading...' }
+        { error && error }
+        { isLoading && <h1>Loading...</h1>}
+        { pokemonData && pokemonData.map((pokemon) => (
+          <Pokemon key={pokemon.name} name={pokemon.name} />))}
       </div>
     </div>
   );
