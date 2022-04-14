ecf58bee08880a87d9243ae31faeb1d721079c2b
Maciej <macmro3@st.amu.edu.pl>
2022-04-14 12:28:39 +0200

    add search bar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,33 @@
-function Navbar() {
+import { useState } from 'react'
+
+function Navbar(props) {
+    const { handleSubmit, refresh } = props
+    const [ pokemonName, setPokemonName ] = useState('')
+
+    //console.log(pokemonName)
+
+    function handleInput(event) {
+        setPokemonName(event.target.value)
+    }
+
+    function onSubmit(event) {
+        event.preventDefault()
+        handleSubmit(pokemonName)
+    }
+    //<input type="submit" onClick={onSubmit} />
     return (
         <nav>
-            <h1>Pokedex</h1>
+            <ul>
+                <li>
+                    <button className="logo" onClick={refresh}>Pokedex</button>
+                </li>
+                <li>
+                    <form className="search--bar" onSubmit={onSubmit}>
+                        <input type="text" name="pokemonName" onChange={handleInput} placeholder="Search..." />
+                    </form>
+                </li>
+            </ul>
+            
         </nav>
     )
 }
